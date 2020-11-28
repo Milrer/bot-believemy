8d163972efbb23ac7d3c3a28d2ef24dfa30a2fa4
Pseudo_Illyes <50676701+PseudoIllyes@users.noreply.github.com>
2020-11-28 20:09:25 +0100

    🔨 | Minimal patch

--- Events/message.js
+++ Events/message.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
-const prefix = '/';
 
 module.exports = (client, message) => {
+    const prefix = client.prefix;
     if (message.author.bot || message.channel.type === 'dm') { return; }
     if (!message.channel.permissionsFor(client.user).has('SEND_MESSAGES')) { return; }
     if (!message.content.startsWith(prefix)) { return; }
