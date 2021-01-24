8cf32d1fe871f73ebec0d059d674f74f1ad7a7fc
Milrer <nicolasleuillet@gmail.com>
2021-01-24 17:09:54 +0100

    🔨 | Utilisation de la variable d'environnement sur Heroku pour la commande "count"

--- Commands/statistics/count.js
+++ Commands/statistics/count.js
@@ -1,8 +1,6 @@
-const GUILD_ID = require("../../Configs/token.js").guildId;
-
 module.exports.run = (client, message, args) => {
 
-    const guild         = client.guilds.cache.get(GUILD_ID);
+    const guild         = client.guilds.cache.get(process.env.guildId);
     const memberCount   = guild.memberCount;
 
     message.channel.send('Nous sommes en ce moment **' + memberCount + '** sur le serveur !');
