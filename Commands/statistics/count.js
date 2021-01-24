76fc70697a4c93ec3ff555d583c54bc68603248d
Milrer <nicolasleuillet@gmail.com>
2021-01-24 17:13:37 +0100

    🔨 | Renommage "guildId" : "guild_id"

--- Commands/statistics/count.js
+++ Commands/statistics/count.js
@@ -1,6 +1,6 @@
 module.exports.run = (client, message, args) => {
 
-    const guild         = client.guilds.cache.get(process.env.guildId);
+    const guild         = client.guilds.cache.get(process.env.guild_id);
     const memberCount   = guild.memberCount;
 
     message.channel.send('Nous sommes en ce moment **' + memberCount + '** sur le serveur !');
