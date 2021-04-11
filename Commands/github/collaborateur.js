2ed930c787d15b646680a252dab802939d7c3bcd
Milrer <nicolasleuillet@gmail.com>
2021-04-11 18:42:50 +0200

    Modification des messages pour les commandes

--- Commands/github/collaborateur.js
+++ Commands/github/collaborateur.js
@@ -1,35 +1,45 @@
-const Discord = require('discord.js');
-const axios = require('axios')
+const Discord = require("discord.js");
+const axios = require("axios");
 module.exports.run = (client, message, args) => {
+  let collabList;
 
-    let collabList;
+  collabList = axios
+    .get("https://api.github.com/repos/Milrer/bot-believemy/contributors")
+    .then((r) => {
+      collabList = r.data;
 
-    collabList = axios.get('https://api.github.com/repos/Milrer/bot-believemy/contributors').then(r => {
-        collabList = r.data;
+      collabList.forEach((u) => {});
 
-        collabList.forEach(u => {
-        })
+      collabList.map((u) => `\n${u.login}`);
 
-        collabList.map(u => `\n${u.login}`);
+      console.log();
 
-        console.log()
+      const date = new Date();
 
-        const date = new Date();
+      const embed = new Discord.MessageEmbed()
+        .setTitle("`🔗` Les collaborateurs")
+        .setColor(`613bdb`)
+        .setURL("https://github.com/Milrer/bot-believemy")
+        .setDescription(
+          `**C'est grâce à eux que BeBot existe aujourd'hui** :\n${collabList
+            .map((u) => `\n> ${u.login}`)
+            .join(
+              "",
+            )}\n\nSi toi aussi tu veux contribuer, viens sur le github (${
+            client.prefix
+          }github)`,
+        )
+        .setFooter(
+          `BeBot @${date.getFullYear()} | believemy.com`,
+          client.user.avatarURL(),
+        );
 
-        const embed = new Discord.MessageEmbed()
-            .setTitle('`🔗` Les collaborateurs')
-            .setColor(`613bdb`)
-            .setURL('https://github.com/Milrer/bot-believemy')
-            .setDescription(`**C'est grâce à eux que le bot existe aujourd'hui** ! Voici la liste de ces **goats** :\n${collabList.map(u => `\n> ${u.login}`).join('')}\n\nSi toi aussi tu veux contribuer, viens sur le github (${client.prefix}github)`)
-            .setFooter(`BeBot @${date.getFullYear()} | believemy.com`, client.user.avatarURL())
-
-        message.channel.send(embed);
+      message.channel.send(embed);
     });
-
-}
+};
 
 module.exports.help = {
-    name: 'collaborateur',
-    aliases: ['collab', 'collaborator'],
-    description: 'Affiche la liste des bg qui ont fait le bot'
-}
+  name: "collaborateur",
+  aliases: ["collab", "collaborator"],
+  description: "Affiche la liste des collaborateurs qui ont élaboré le BeBot",
+};
