6301f33ef2cec90ef4374659a56dab12ba0f07db
Milrer <nicolasleuillet@gmail.com>
2021-05-10 17:07:56 +0200

    Mise à jour du lien vers la photo de BeBot

--- index.js
+++ index.js
@@ -1,84 +1,84 @@
-const Discord = require("discord.js");
-const fs = require("fs");
-const logger = require("./Configs/logger");
+const Discord = require('discord.js');
+const fs = require('fs');
+const logger = require('./Configs/logger');
 const client = new Discord.Client();
-const config = require("./Configs/config.js");
+const config = require('./Configs/config.js');
 client.prefix = config.prefix;
 let token;
-const cron = require("node-cron");
-const quotesArray = require("./src/quotes.js");
+const cron = require('node-cron');
+const quotesArray = require('./src/quotes.js');
 
 if (process.env && process.env.token) {
-  token = process.env.token;
+    token = process.env.token;
 } else {
-  token = require("./Configs/token").token;
+    token = require('./Configs/token').token;
 }
 
-client.login(token).then(logger.log(`Bot démarré`, "log"));
+client.login(token).then(logger.log(`Bot démarré`, 'log'));
 
 // Citation quotidienne du matin
-cron.schedule("45 7 * * *", function () {
-  let channel = client.channels.cache.get("770587361058488340");
-  let date = new Date();
-  const options = {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "2-digit",
-  };
-  date = date.toLocaleDateString("fr-FR", options);
-  const quote = quotesArray[Math.floor(Math.random() * quotesArray.length)];
-  const embed = new Discord.MessageEmbed();
-  embed
-    .setAuthor("BeBot", "https://believemy.com/pictures/brand/favicon_1000.png")
-    .setDescription(`${quote.citation}`)
-    .setColor("613bdb")
-    .setTitle(`Nous sommes le ${date}`)
-    .setFooter(`${quote.nom}`, quote.image);
-  channel.send(embed);
+cron.schedule('45 7 * * *', function () {
+    let channel = client.channels.cache.get('770587361058488340');
+    let date = new Date();
+    const options = {
+        weekday: 'long',
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit',
+    };
+    date = date.toLocaleDateString('fr-FR', options);
+    const quote = quotesArray[Math.floor(Math.random() * quotesArray.length)];
+    const embed = new Discord.MessageEmbed();
+    embed
+        .setAuthor('BeBot', 'https://believemy.com/pictures/bebot/bebot-profile.png')
+        .setDescription(`${quote.citation}`)
+        .setColor('613bdb')
+        .setTitle(`Nous sommes le ${date}`)
+        .setFooter(`${quote.nom}`, quote.image);
+    channel.send(embed);
 });
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 
-fs.readdirSync("./Commands/").forEach((dir) => {
-  const commands = fs
-    .readdirSync(`./Commands/${dir}/`)
-    .filter((file) => file.endsWith(".js"));
-  for (let file of commands) {
-    let pull = require(`./Commands/${dir}/${file}`);
-    if (pull.help.name) {
-      client.commands.set(pull.help.name, pull);
-      logger.log(`La commande ${pull.help.name} a bien été chargée`, "ready");
-    } else {
-      logger.log(
-        `La commande ${file} n'est pas chargée, il doit manquer une info dans le module 'help'`,
-        "error",
-      );
+fs.readdirSync('./Commands/').forEach(dir => {
+    const commands = fs
+        .readdirSync(`./Commands/${dir}/`)
+        .filter(file => file.endsWith('.js'));
+    for (let file of commands) {
+        let pull = require(`./Commands/${dir}/${file}`);
+        if (pull.help.name) {
+            client.commands.set(pull.help.name, pull);
+            logger.log(`La commande ${pull.help.name} a bien été chargée`, 'ready');
+        } else {
+            logger.log(
+                `La commande ${file} n'est pas chargée, il doit manquer une info dans le module 'help'`,
+                'error',
+            );
+        }
+        if (pull.help.aliases) {
+            pull.help.aliases.forEach(alias => {
+                client.aliases.set(alias, pull.help.name);
+            });
+        }
     }
-    if (pull.help.aliases) {
-      pull.help.aliases.forEach((alias) => {
-        client.aliases.set(alias, pull.help.name);
-      });
-    }
-  }
 });
 
-fs.readdir("./Events/", (error, f) => {
-  if (error) {
-    return console.error(error);
-  }
-  let event = f.filter((f) => f.split(".").pop() === "js");
-  if (event.length <= 0) {
-    return logger.log("Aucun event trouvé !", "error");
-  } else {
-    logger.log(`${f.length} events chargés`, "info");
-  }
+fs.readdir('./Events/', (error, f) => {
+    if (error) {
+        return console.error(error);
+    }
+    let event = f.filter(f => f.split('.').pop() === 'js');
+    if (event.length <= 0) {
+        return logger.log('Aucun event trouvé !', 'error');
+    } else {
+        logger.log(`${f.length} events chargés`, 'info');
+    }
 
-  f.forEach((f) => {
-    let events = require(`./Events/${f}`);
-    let event = f.split(".")[0];
-    client.on(event, events.bind(null, client));
-    logger.log(`EVENT | ${event} CHARGÉ`, "ready");
-  });
+    f.forEach(f => {
+        let events = require(`./Events/${f}`);
+        let event = f.split('.')[0];
+        client.on(event, events.bind(null, client));
+        logger.log(`EVENT | ${event} CHARGÉ`, 'ready');
+    });
 });
