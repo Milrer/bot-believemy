f9ab073c1baa1c1d3733366dce916a11200adbfa
Milrer <nicolasleuillet@gmail.com>
2021-05-11 16:16:03 +0200

    -

--- index.js
+++ index.js
@@ -18,35 +18,9 @@ if (process.env && process.env.token) {
 client.login(token).then(logger.log(`Bot démarré`, 'log'));
 
 // Citation quotidienne du matin
-function happyDay() {
+// cron.schedule('45 7 * * *', async () => {
+cron.schedule('*/3 * * * *', async () => {
     await Axios.get(
-        'https://fetedujour.fr/api/v2/JVVPdIFBvcdgNyEf/json-saints?api_key=JVVPdIFBvcdgNyEf',
-    ).then(response => {
-        let channel = client.channels.cache.get('770587361058488340');
-        let date = new Date();
-        const options = {
-            weekday: 'long',
-            year: 'numeric',
-            month: 'long',
-            day: '2-digit',
-        };
-        date = date.toLocaleDateString('fr-FR', options);
-        const quote = quotesArray[Math.floor(Math.random() * quotesArray.length)];
-        const name = response.saints.name;
-        const embed = new Discord.MessageEmbed();
-        embed
-            .setAuthor('BeBot', 'https://believemy.com/pictures/bebot/bebot-profile.png')
-            // .setDescription(`${quote.citation}`)
-            .setDescription(`Bonne fête à tous les *${name}*.`)
-            .setColor('613bdb')
-            .setTitle(`Nous sommes le ${date}`)
-            .setFooter(`${quote.nom}`, quote.image);
-        channel.send(embed);
-    });
-}
-// cron.schedule('45 7 * * *', happyDay());
-cron.schedule('*/3 * * * *', () => {
-    Axios.get(
         'https://fetedujour.fr/api/v2/JVVPdIFBvcdgNyEf/json-saints?api_key=JVVPdIFBvcdgNyEf',
     ).then(response => {
         let channel = client.channels.cache.get('770587361058488340');
