4f5393d9539ec7c0a35a1acf4b3f9b54678e5745
Louis-Nicolas Leuillet <nicolasleuillet@gmail.com>
2022-01-21 22:15:19 +0100

    -

--- index.js
+++ index.js
@@ -1,93 +1,106 @@
-const Discord = require('discord.js');
-const fs = require('fs');
-const logger = require('./Configs/logger');
+const Discord = require("discord.js");
+const fs = require("fs");
+const logger = require("./Configs/logger");
 const client = new Discord.Client();
-const config = require('./Configs/config.js');
+const config = require("./Configs/config.js");
 client.prefix = config.prefix;
 let token;
-const cron = require('node-cron');
-const quotesArray = require('./src/quotes.js');
-const Axios = require('axios');
-const moment = require('moment');
-require('moment/locale/fr');
+const cron = require("node-cron");
+const quotesArray = require("./src/quotes.js");
+const Axios = require("axios");
+const moment = require("moment");
+require("moment/locale/fr");
 
 if (process.env && process.env.token) {
-    token = process.env.token;
+	token = process.env.token;
 } else {
-    token = require('./Configs/token').token;
+	token = require("./Configs/token").token;
 }
 
-client.login(token).then(logger.log(`Bot démarré`, 'log'));
+client.login(token).then(logger.log(`Bot démarré`, "log"));
 
 // Citation quotidienne du matin
-cron.schedule('45 7 * * *', () => {
-    Axios.get(
-        'https://fetedujour.fr/api/v2/JVVPdIFBvcdgNyEf/json-saint?api_key=JVVPdIFBvcdgNyEf',
-    )
-        .then(response => {
-            let channel = client.channels.cache.get('749242783058886719');
-            moment.locale('fr');
-            const date = moment().format('dddd Do MMMM YYYY');
-            // const quote = quotesArray[Math.floor(Math.random() * quotesArray.length)];
-            const embed = new Discord.MessageEmbed();
-            embed
-                .setAuthor(
-                    'BeBot',
-                    'https://believemy.com/pictures/bebot/bebot-profile.png',
-                )
-                // .setDescription(`${quote.citation}`)
-                .setDescription(
-                    `Nous fêtons les **${response.data.name}** aujourd'hui, bonne journée à tous !`,
-                )
-                .setColor('613bdb')
-                .setTitle(`Nous sommes le ${date}`);
-            // .setFooter(`${quote.nom}`, quote.image);
-            channel.send(embed);
-        })
-        .catch(error => console.log(error));
+cron.schedule("*/5 * * * *", () => {
+	Axios.get(
+		"https://fetedujour.fr/api/v2/JVVPdIFBvcdgNyEf/json-saint?api_key=JVVPdIFBvcdgNyEf",
+	)
+		.then((response) => {
+			Axios.get(
+				"https://newsapi.org/v2/top-headlines?language=fr&category=technology&apiKey=22e993978b4043b0b16df2e1aaf44c3f&pageSize=3",
+			)
+				.then((responses) => {
+					let channel = client.channels.cache.get(
+						"770588039306018838",
+					);
+					moment.locale("fr");
+					const date = moment().format("dddd Do MMMM YYYY");
+					// const quote = quotesArray[Math.floor(Math.random() * quotesArray.length)];
+					const embed = new Discord.MessageEmbed();
+					embed
+						.setAuthor(
+							"BeBot",
+							"https://believemy.com/pictures/bebot/bebot-profile.png",
+						)
+						// .setDescription(`${quote.citation}`)
+						.setDescription(
+							`Nous fêtons les **${response.data.name}** aujourd'hui, bonne journée à tous !`,
+						)
+						.setColor("613bdb")
+						.setTitle(`Nous sommes le ${date}`);
+					// .setFooter(`${quote.nom}`, quote.image);
+					channel.send(embed);
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		})
+		.catch((error) => console.log(error));
 });
 
 client.commands = new Discord.Collection();
 client.aliases = new Discord.Collection();
 
-fs.readdirSync('./Commands/').forEach(dir => {
-    const commands = fs
-        .readdirSync(`./Commands/${dir}/`)
-        .filter(file => file.endsWith('.js'));
-    for (let file of commands) {
-        let pull = require(`./Commands/${dir}/${file}`);
-        if (pull.help.name) {
-            client.commands.set(pull.help.name, pull);
-            logger.log(`La commande ${pull.help.name} a bien été chargée`, 'ready');
-        } else {
-            logger.log(
-                `La commande ${file} n'est pas chargée, il doit manquer une info dans le module 'help'`,
-                'error',
-            );
-        }
-        if (pull.help.aliases) {
-            pull.help.aliases.forEach(alias => {
-                client.aliases.set(alias, pull.help.name);
-            });
-        }
-    }
+fs.readdirSync("./Commands/").forEach((dir) => {
+	const commands = fs
+		.readdirSync(`./Commands/${dir}/`)
+		.filter((file) => file.endsWith(".js"));
+	for (let file of commands) {
+		let pull = require(`./Commands/${dir}/${file}`);
+		if (pull.help.name) {
+			client.commands.set(pull.help.name, pull);
+			logger.log(
+				`La commande ${pull.help.name} a bien été chargée`,
+				"ready",
+			);
+		} else {
+			logger.log(
+				`La commande ${file} n'est pas chargée, il doit manquer une info dans le module 'help'`,
+				"error",
+			);
+		}
+		if (pull.help.aliases) {
+			pull.help.aliases.forEach((alias) => {
+				client.aliases.set(alias, pull.help.name);
+			});
+		}
+	}
 });
 
-fs.readdir('./Events/', (error, f) => {
-    if (error) {
-        return console.error(error);
-    }
-    let event = f.filter(f => f.split('.').pop() === 'js');
-    if (event.length <= 0) {
-        return logger.log('Aucun event trouvé !', 'error');
-    } else {
-        logger.log(`${f.length} events chargés`, 'info');
-    }
+fs.readdir("./Events/", (error, f) => {
+	if (error) {
+		return console.error(error);
+	}
+	let event = f.filter((f) => f.split(".").pop() === "js");
+	if (event.length <= 0) {
+		return logger.log("Aucun event trouvé !", "error");
+	} else {
+		logger.log(`${f.length} events chargés`, "info");
+	}
 
-    f.forEach(f => {
-        let events = require(`./Events/${f}`);
-        let event = f.split('.')[0];
-        client.on(event, events.bind(null, client));
-        logger.log(`EVENT | ${event} CHARGÉ`, 'ready');
-    });
+	f.forEach((f) => {
+		let events = require(`./Events/${f}`);
+		let event = f.split(".")[0];
+		client.on(event, events.bind(null, client));
+		logger.log(`EVENT | ${event} CHARGÉ`, "ready");
+	});
 });
