4e1b2858193bf0a245c3d049c24a70aeb08e7280
Louis-Nicolas Leuillet <nicolasleuillet@gmail.com>
2022-09-20 09:24:19 +0200

    test

--- index.js
+++ index.js
@@ -20,6 +20,8 @@ if (process.env && process.env.token) {
 
 client.login(token).then(logger.log(`Bot démarré`, "log"));
 
+console.log(ephmeris.getEphemerisName());
+
 // Bonjour du matin
 cron.schedule("23 9 * * *", () => {
 	let channel = client.channels.cache.get("749242783058886719");
