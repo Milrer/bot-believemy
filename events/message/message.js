cb84e10fc64c054d32dd314b5a9111732fa081db
SebastienWeb <sebastienweb27@gmail.com>
2023-06-03 19:54:26 +0200

    ♻️ Add regex anti spam link

--- events/message/message.js
+++ events/message/message.js
@@ -1,31 +1,34 @@
 import { Events } from "discord.js";
 
 export default {
-    name: Events.MessageCreate,
-    on: true,
-    async execute(message) {
-        if (message.author.bot) {
-            return;
-        }
-        if (
-            message.author.id === "286174545969283094" ||
-            message.member.roles.cache.has("1111224353804263425") ||
-            message.member.roles.cache.has("1111212236883644567")
-        ) {
-            return;
-        } else if (
-            message.content.includes("https://") ||
-            message.content.includes("discord.gg") ||
-            message.content.includes("http://")
-        ) {
-            await message.delete();
-            return message.channel
-                .send({
-                    content: `${message.author}, il est interdit de poster des liens : vous devez avoir le rôle Développeur ou Entrepreneur pour cela.`,
-                })
-                .then((msg) => {
-                    setTimeout(() => msg.delete(), 5000);
-                });
-        }
-    },
+  name: Events.MessageCreate,
+  on: true,
+  async execute(message) {
+    const regex = /\[[\w\s]+\]/;
+    if (message.author.bot) {
+      return;
+    }
+    if (
+      message.author.id === "286174545969283094" ||
+      message.member.roles.cache.has("1111224353804263425") ||
+      message.member.roles.cache.has("1111212236883644567")
+    ) {
+      return;
+    } else if (regex.test(message.content)) {
+      return;
+    } else if (
+      message.content.includes("https://") ||
+      message.content.includes("discord.gg") ||
+      message.content.includes("http://")
+    ) {
+      await message.delete();
+      return message.channel
+        .send({
+          content: `${message.author}, il est interdit de poster des liens : vous devez avoir le rôle Développeur ou Entrepreneur pour cela.`,
+        })
+        .then((msg) => {
+          setTimeout(() => msg.delete(), 5000);
+        });
+    }
+  },
 };
