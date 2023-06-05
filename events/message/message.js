96d7cb64349ad3baddd9b854615c6f30f3fd36fa
SebastienWeb <sebastienweb27@gmail.com>
2023-06-05 16:08:20 +0200

    Refacto spam link

--- events/message/message.js
+++ events/message/message.js
@@ -1,53 +1,41 @@
 import { Events } from "discord.js";
 
 export default {
-    name: Events.MessageCreate,
-    on: true,
-    async execute(message) {
-        // if (
-        //   message.author.id === "286174545969283094" ||
-        //   message.member.roles.cache.has("1111224353804263425") ||
-        //   message.member.roles.cache.has("1111212236883644567")
-        // ) {
-        //   return;
-        // }
-        const whiteList = [
-            "https://tenor.com/",
-            "https://www.tenor.com",
-            "https://believemy.com",
-            "https://www.believemy.com",
-        ];
-        const link = ["https://discord.gg/", "discord.gg"];
-        const regex = /\[[\w\s]+\]/;
+  name: Events.MessageCreate,
+  on: true,
+  async execute(message) {
+    const regex = /\[[\w\s]+\]/;
+    const whiteList = [
+      "https://tenor.com/",
+      "https://www.tenor.com",
+      "https://believemy.com",
+      "https://www.believemy.com",
+    ];
+    const isWhitelisted = whiteList.some((whiteLink) =>
+      message.content.includes(whiteLink)
+    );
+    const isBlacklisted = ["https://", "http://", "discord.gg"].some(
+      (blackLink) => message.content.includes(blackLink) && !isWhitelisted
+    );
 
-        if (message.content.includes(whiteList)) {
-            return;
-        } else if (message.content.includes(link)) {
-            await message.delete();
-            return message.channel
-                .send({
-                    content: `${message.author}, merci de bien formater le lien, [nom de la ressource] votre lien`,
-                })
-                .then((msg) => {
-                    setTimeout(() => msg.delete(), 5000);
-                });
-        } else if (message.author.bot) {
-            return;
-        } else if (regex.test(message.content)) {
-            return;
-        } else if (
-            message.content.includes("https://") ||
-            message.content.includes("discord.gg") ||
-            message.content.includes("http://")
-        ) {
-            await message.delete();
-            return message.channel
-                .send({
-                    content: `${message.author}, merci de bien formater le lien, [nom de la ressource] votre lien`,
-                })
-                .then((msg) => {
-                    setTimeout(() => msg.delete(), 5000);
-                });
-        }
-    },
+    if (message.author.bot) {
+      return;
+    }
+    if (isWhitelisted) {
+      return;
+    }
+    if (regex.test(message.content)) {
+      return;
+    }
+    if (isBlacklisted) {
+      await message.delete();
+      return message.channel
+        .send({
+          content: `${message.author}, merci de bien formater le lien, [nom de la ressource] votre lien`,
+        })
+        .then((msg) => {
+          setTimeout(() => msg.delete(), 5000);
+        });
+    }
+  },
 };
