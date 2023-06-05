c526b9eeda9e3ba29c3b99e1a35f2ea865e61df6
Louis-Nicolas <nicolasleuillet@gmail.com>
2023-06-05 16:12:52 +0200

    Test with mozilla org

--- events/message/message.js
+++ events/message/message.js
@@ -1,41 +1,43 @@
 import { Events } from "discord.js";
 
 export default {
-  name: Events.MessageCreate,
-  on: true,
-  async execute(message) {
-    const regex = /\[[\w\s]+\]/;
-    const whiteList = [
-      "https://tenor.com/",
-      "https://www.tenor.com",
-      "https://believemy.com",
-      "https://www.believemy.com",
-    ];
-    const isWhitelisted = whiteList.some((whiteLink) =>
-      message.content.includes(whiteLink)
-    );
-    const isBlacklisted = ["https://", "http://", "discord.gg"].some(
-      (blackLink) => message.content.includes(blackLink) && !isWhitelisted
-    );
+    name: Events.MessageCreate,
+    on: true,
+    async execute(message) {
+        const regex = /\[[\w\s]+\]/;
+        const whiteList = [
+            "https://tenor.com/",
+            "https://www.tenor.com",
+            "https://believemy.com",
+            "https://www.believemy.com",
+            "mozilla.org",
+        ];
+        const isWhitelisted = whiteList.some((whiteLink) =>
+            message.content.includes(whiteLink),
+        );
+        const isBlacklisted = ["https://", "http://", "discord.gg"].some(
+            (blackLink) =>
+                message.content.includes(blackLink) && !isWhitelisted,
+        );
 
-    if (message.author.bot) {
-      return;
-    }
-    if (isWhitelisted) {
-      return;
-    }
-    if (regex.test(message.content)) {
-      return;
-    }
-    if (isBlacklisted) {
-      await message.delete();
-      return message.channel
-        .send({
-          content: `${message.author}, merci de bien formater le lien, [nom de la ressource] votre lien`,
-        })
-        .then((msg) => {
-          setTimeout(() => msg.delete(), 5000);
-        });
-    }
-  },
+        if (message.author.bot) {
+            return;
+        }
+        if (isWhitelisted) {
+            return;
+        }
+        if (regex.test(message.content)) {
+            return;
+        }
+        if (isBlacklisted) {
+            await message.delete();
+            return message.channel
+                .send({
+                    content: `${message.author}, merci de bien formater le lien, [nom de la ressource] votre lien`,
+                })
+                .then((msg) => {
+                    setTimeout(() => msg.delete(), 5000);
+                });
+        }
+    },
 };
