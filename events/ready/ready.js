e90951c06eb30ec1c465cbb8b999a7d4301759a0
SebastienWeb <sebastienweb27@gmail.com>
2023-09-07 15:15:15 +0200

    Erreur corrigé

--- events/ready/ready.js
+++ events/ready/ready.js
@@ -1,32 +1,15 @@
 import { ActivityType, Events } from 'discord.js';
 import { registerCommands } from '../../helpers/registerCommands.js';
-import {
-    getFollowersCount,
-    getUserId,
-} from '../../helpers/twitch/followersCount.js';
-
 export default {
     name: Events.ClientReady,
     once: true,
     async execute(client) {
         console.log(`Ready! Logged in as ${client.user.tag}`);
-
+        client.user.setActivity('tout le monde.', {
+            type: ActivityType.Watching,
+        });
         try {
-            await registerCommands(client);
-            const twitchUserId = await getUserId(
-                process.env.PSEUDO_TWITCH,
-                process.env.TOKEN_TWITCH
-            );
-
-            if (twitchUserId) {
-                const followersCount = await getFollowersCount(
-                    twitchUserId,
-                    process.env.TOKEN_TWITCH
-                );
-                client.user.setActivity(`avec ${followersCount} followers`, {
-                    type: ActivityType.Playing,
-                });
-            }
+            registerCommands(client);
         } catch (error) {
             console.log(error);
         }
