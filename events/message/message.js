39e20680dff5f5280844d6fa23f6def5dbcf46d9
SebastienWeb <sebastienweb27@gmail.com>
2023-09-11 12:05:02 +0200

    Add localhost passed

--- events/message/message.js
+++ events/message/message.js
@@ -1,25 +1,39 @@
-import { Events } from "discord.js";
-import { blacklist } from "../../blacklist/blacklist.js";
+import { Events } from 'discord.js';
+import { blacklist } from '../../blacklist/blacklist.js';
 
 export default {
-  name: Events.MessageCreate,
-  on: true,
-  async execute(message) {
-    if (message.author.bot) {
-      return;
-    }
-    const isBlacklisted = blacklist.some((blackLink) =>
-      message.content.includes(blackLink)
-    );
-    if (isBlacklisted) {
-      await message.delete();
-      return message.channel
-        .send({
-          content: `${message.author}, Liens interdit`,
-        })
-        .then((msg) => {
-          setTimeout(() => msg.delete(), 5000);
-        });
-    }
-  },
+    name: Events.MessageCreate,
+    on: true,
+    async execute(message) {
+        if (message.author.bot) {
+            return;
+        }
+        const isBlacklisted = blacklist.some((blackLink) =>
+            message.content.includes(blackLink)
+        );
+        if (
+            message.content == 'http://localhost' ||
+            message.content == 'http://localhost/' ||
+            message.content == 'http://localhost:3000' ||
+            message.content == 'http://localhost:3000/' ||
+            message.content == 'http://localhost:5173' ||
+            message.content == 'http://localhost:5173/' ||
+            message.content == 'http://localhost:8080' ||
+            message.content == 'http://localhost:8080/' ||
+            message.content == 'http://localhost:8000' ||
+            message.content == 'http://localhost:8000/'
+        ) {
+            return;
+        }
+        if (isBlacklisted) {
+            await message.delete();
+            return message.channel
+                .send({
+                    content: `${message.author}, Liens interdit`,
+                })
+                .then((msg) => {
+                    setTimeout(() => msg.delete(), 5000);
+                });
+        }
+    },
 };
