bfdebe28444137707e5b75a9367c447dd91c8940
Louis-Nicolas <nicolasleuillet@gmail.com>
2026-01-16 12:08:38 +0100

    Message pour les ateliers

--- helpers/workshopNotifications.js
+++ helpers/workshopNotifications.js
@@ -74,7 +74,6 @@ function createAnnouncementEmbed(workshop, client) {
         color: WORKSHOP_COLOR,
         title: `Nouvel atelier : ${workshop.title}`,
         description: truncateDescription(workshop.description),
-        image: workshop.thumbnail ? { url: workshop.thumbnail } : undefined,
         fields: [
             {
                 name: 'Date',
@@ -130,7 +129,6 @@ function createReminderEmbed(workshop, client) {
         description: `**${workshop.title}** commence à ${formatTime(
             workshop.startDate
         )} (heure de Paris) !`,
-        image: workshop.thumbnail ? { url: workshop.thumbnail } : undefined,
         fields: [
             {
                 name: 'Horaire (heure de Paris)',
@@ -177,7 +175,6 @@ function createReplayEmbed(workshop, client) {
         color: WORKSHOP_COLOR,
         title: `Replay disponible : ${workshop.title}`,
         description: "Le replay de l'atelier est maintenant disponible !",
-        image: workshop.thumbnail ? { url: workshop.thumbnail } : undefined,
         fields: [
             {
                 name: `${getSpeakerLabel(workshop.speakers)}`,
@@ -229,8 +226,14 @@ async function sendWorkshopNotification(client, workshop, createEmbedFn, type) {
     }
 
     const { embed, row } = createEmbedFn(workshop, client);
+
+    // Construire le contenu avec mention + image (si disponible)
+    const content = workshop.thumbnail
+        ? `${roleMention}\n${workshop.thumbnail}`
+        : roleMention;
+
     await channel.send({
-        content: roleMention,
+        content,
         embeds: [embed],
         components: [row],
     });
