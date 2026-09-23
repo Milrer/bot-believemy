0dd0f32bfd5e6cc288878eca8af2ec6f4dd9ce59
Louis-Nicolas <nicolasleuillet@gmail.com>
2026-09-23 18:20:44 +0200

    BeBot ne souhaite plus les anniversaires
    La fonctionnalité est retirée entièrement, sur décision de Louis-Nicolas. Elle
    plantait de toute façon pour deux des trois figures fêtées : leurs messages
    étaient tirés au hasard dans un tableau déclaré dans le bloc d'une autre, donc
    introuvable au moment de l'appel.

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 import { registerEvents } from './helpers/registerEvent.js';
 import { ephemerisRepeat } from './helpers/ephemerisRepeat.js';
-import { birthday } from './helpers/birthday.js';
 import { serverCover } from './helpers/serverCover.js';
 import { workshopNotifications } from './helpers/workshopNotifications.js';
 import { presence } from './helpers/presence.js';
@@ -18,7 +17,6 @@ const client = new Client({
 
 await registerEvents(client);
 ephemerisRepeat(client);
-birthday(client);
 serverCover(client);
 workshopNotifications(client);
 presence(client);
